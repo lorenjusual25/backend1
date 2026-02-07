639ddb1652926b2cd6560240094b0f2ed77ac485
Lorenzo Julian Suarez Almeyra <lsuareza2005@gmail.com>
2026-02-07 13:00:58 -0300

    entrega

---
+++ actividad/actividad1.js
@@ -0,0 +1,57 @@
+console.log("hola desde aqui")
+class ProductManager {
+    constructor () {
+        this.products = []
+    }
+    addProduct (product) {
+        if (!product.title || !product.description || !product.price || !product.thumbnail || !product.id || !product.stock || !product.status || !product.category) {
+            console.error("Faltan campos")
+            return
+        }
+        if (this.products.some(p => p.id === product.id)){
+            console.error("ID repetido")
+            return
+        }
+        this.products.push(product)
+    }
+    getProducts() {
+        return this.products
+    }
+    getProductByID (id) {
+        const producto = this.products.find(p => p.id === id)
+        if (!producto){
+            console.error("not found")
+            return
+        }
+        return producto
+    }
+}
+export default ProductManager
+/*let i = 0
+let prod = new ProductManager()
+prod.addProduct({
+    title: "titulo1",
+    description: "desc1",
+    price: 1,
+    thumbnail: "tn1",
+    id: ++i,
+    stock: 1
+})
+prod.addProduct({
+    title: "titulo2",
+    description: "desc2",
+    price: 2,
+    thumbnail: "tn2",
+    id: ++i,
+    stock: 2
+})
+prod.addProduct({
+    title: "titulo3",
+    description: "desc3",
+    price: 3,
+    thumbnail: "tn3",
+    id: ++i,
+    stock: 3
+})
+console.log(prod.getProducts())
+prod.getProductByID(5)*/
---
+++ actividad/cartmanager.js
@@ -0,0 +1,60 @@
+import fs from "fs"
+class CartManager {
+    constructor() {
+        this.path = "./carts.json"
+        this.carts = this.cargar()
+    }
+    cargar() {
+        try {
+            if (fs.existsSync(this.path)) {
+                const data = fs.readFileSync(this.path, "utf-8")
+                return JSON.parse(data)
+            }
+            return []
+        }
+        catch (error) {
+            console.error("Error al cargar el carrito:", error)
+            return []
+        }
+    }
+    createCart() {
+        const newCart = {
+            id: this.carts.length + 1,
+            products: []
+        }
+        this.carts.push(newCart)
+        this.guardar()
+        return newCart
+    }
+    getCartByID(id) {
+        const cart = this.carts.find(c => c.id === id)
+        if (!cart) {
+            console.error("Carrito no encontrado")
+            return null
+        }
+        return cart
+    }
+    guardar() {
+        try {
+            fs.writeFileSync(this.path, JSON.stringify(this.carts, null, 2))
+        } catch (error) {
+            console.error("Error al guardar el carrito:", error)
+        }
+    }
+    addProductToCart(cartId, productId) {
+        const cart = this.getCartByID(cartId)
+        if (!cart) {
+            console.error("Carrito no encontrado")
+            return null
+        }
+        const productInCart = cart.products.find(p => p.product === productId)
+        if (productInCart) {
+            productInCart.quantity++
+        } else {
+            cart.products.push({ product: productId, quantity: 1 })
+        }
+        this.guardar()
+        return cart
+    }
+}
+export default CartManager
